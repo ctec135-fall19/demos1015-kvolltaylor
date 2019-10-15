1df7bf551a089190a71f4c0b23c7be6c747983b2
Katrina <k.voll-taylor@students.clark.edu>
2019-10-15 15:21:03 -0700

    Tuesday Lab Submission

---
+++ Demo1015/Inheritance/BaseClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class BaseClass
+    {
+
+        // fields
+        private int basePrivateInt;
+        protected int baseProtectedInt;
+
+        // constructors
+
+        public BaseClass() { }
+
+        public BaseClass (int privateInt, int protectedInt)
+        {
+            this.basePrivateInt = privateInt;
+            this.baseProtectedInt = protectedInt;
+        }
+
+        //methods
+        public void BaseMethod()
+        {
+            Console.WriteLine("BaseClass.BaseMethod");
+        }
+
+        // adding the word virtual 
+        // virtual says i may override this
+        // tells compiler I have children classes that may but don't have
+        // to override this method
+        public virtual void PrintState()
+        {
+            Console.WriteLine("BaseClass object");
+            Console.WriteLine("\tbasePrivateInt: {0}", basePrivateInt);
+            Console.WriteLine("\tbaseProtectedInt: {0}", baseProtectedInt);
+        }
+    }
+}
---
+++ Demo1015/Inheritance/ChildClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class ChildClass : BaseClass
+    {
+
+        // fields
+        protected int childProtectedInt;
+
+        // constructors
+        public ChildClass() { }
+
+        public ChildClass(int basePrivateInt, int baseProtectedInt, int childProtectedInt)
+            // these two are using inherited constructors from BaseClass
+            : base(basePrivateInt, baseProtectedInt)
+        {
+            this.childProtectedInt = childProtectedInt;
+        }
+
+        //methods
+        public void ChildMethod()
+        {
+            Console.WriteLine("ChildClass.ChildMethod");
+        }
+
+        // what override does is say i'm redefining this method for the ChildClass
+        // override says I know i've inherited this method but i'm going to 
+        // override what my parent says
+        public override void PrintState()
+        {
+            // including the below writeline can help you tell where you are
+            // in the code and the hierarchy
+            // using the print command can help you debug without using the debugger
+            Console.WriteLine("ChildClass object");
+
+            // demo of access: ChildClass has access to baseProtectedInt but not basePrivateInt
+            //Console.WriteLine("\tbasePrivateInt: {0}", basePrivateInt);
+            //Console.WriteLine("\tbaseProtectedInt: {0}", baseProtectedInt);
+
+            Console.WriteLine("\tchildProtectedInt: {0}", childProtectedInt);
+            base.PrintState();
+        }
+    }
+}
